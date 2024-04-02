15c8db4f8364a616cbc6f895f3f5b587e79a4ec5
Mateusz Tyc <tycmat9@gmail.com>
2024-04-02 17:31:52 -0400

    Add ROSLIB dependency and initialize ROS connection

--- app/index.ts
+++ app/index.ts
@@ -1,15 +1,22 @@
 import express, { Express } from 'express';
 import  { createServer } from  'http';
 import { Server } from 'socket.io';
+import ROSLIB from 'roslib';
+
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+var ros = new ROSLIB.Ros({
+    url: 'ws://localhost:9090'
+});
+
+
 io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 }); 
 
 app.use(express.static('./public'));
