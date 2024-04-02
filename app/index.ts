e7cb775adbd7957d300b370c6f933f9ea06f7e45
Mateusz Tyc <tycmat9@gmail.com>
2024-04-02 17:21:42 -0400

    Add socket.io functionality and update server configuration

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,16 @@
 import express, { Express } from 'express';
+import  { createServer } from  'http';
+import { Server } from 'socket.io';
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-const app: Express = express();
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+}); 
 
 app.use(express.static('./public'));
-app.listen(5000, () => console.log('Server is up and running'));
+httpServer.listen(5000, () => console.log('Server is up and running'));
