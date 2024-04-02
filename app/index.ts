200990d959ede4e238e7778da068116a2f34b95c
Mateusz Tyc <tycmat9@gmail.com>
2024-04-02 16:37:14 -0400

    Add TypeScript support and update Dockerfile

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,6 @@
-const express = require('express');
-const app = express();
+import express, { Express } from 'express';
+
+const app: Express = express();
+
 app.use(express.static('./public'));
 app.listen(5000, () => console.log('Server is up and running'));
