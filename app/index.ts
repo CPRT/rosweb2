00cc498f80996f591c9f1f2e4ab8b2e42bc2d856
Mateusz Tyc <tycmat9@gmail.com>
2024-04-02 18:28:23 -0400

    Update WebSocket implementation and dependencies

--- app/index.ts
+++ app/index.ts
@@ -1,23 +1,35 @@
 import express, { Express } from 'express';
 import  { createServer } from  'http';
-import { Server } from 'socket.io';
 import ROSLIB from 'roslib';
+import WebSocket from 'ws'; 
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const wss = new WebSocket.Server({ 
+    server: httpServer 
+});
 
 var ros = new ROSLIB.Ros({
     url: 'ws://localhost:9090'
 });
 
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+wss.on('connection', function(ws) { 
+    console.log('Connected to websocket server.');
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        ws.send('Message received: ' + message);
     });
-}); 
+});
+
+ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+});
+
+
 
 app.use(express.static('./public'));
 httpServer.listen(5000, () => console.log('Server is up and running'));
